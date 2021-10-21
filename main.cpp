99cabf4160089af413f4e776e5c111b831217580
mariababs <70773854+mariababs@users.noreply.github.com>
2021-10-21 11:37:45 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,49 @@
 
 using namespace std;
 
-int main() {
+// abstract base class
+class Pet { 
+    private:
+        int hunger;
+        int mood;
+    public: 
+        void setHunger(int amount) { 
+            hunger = hunger + amount;
+        }
+        int getHunger() { 
+            return hunger;
+        }
+
+        void setMood(int amount) { 
+            mood = mood + amount;
+        }
+        int getMood() { 
+            return mood;
+        }
+};
+
+class Dog : public Pet { 
+    private: 
+
+    public: 
+
+};
+
+class Cat : public Pet { 
+    private: 
+
+    public: 
     
+};
+
+class Eagle : public Pet { 
+    private: 
+
+    public: 
+
+};
+
+int main() {
+
     return 0;
 }
