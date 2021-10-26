f3f93a0772bdc3873dfcd617476df71c179407ba
mariababs <70773854+mariababs@users.noreply.github.com>
2021-10-26 11:49:58 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,7 +9,12 @@ class Pet {
     private:
         int hunger;
         int mood;
+        string name;
     public: 
+        // pure virtual functions
+        virtual void eat() = 0;
+        virtual void action() = 0;
+
         void setHunger(int amount) { 
             hunger = hunger + amount;
         }
@@ -23,30 +28,99 @@ class Pet {
         int getMood() { 
             return mood;
         }
+
+        void setName(string n) { 
+            name = n;
+        }
+        string getName() { 
+            return name;
+        }
+
+        string decideMood() {
+            if (mood <= 100 && mood >= 90) {
+                return "Excited";
+            } else if (mood <= 89 && mood >= 80) {
+                return "Happy";
+            } else if (mood <= 79 && mood >= 70) {
+                return "Calm";
+            } else if (mood <= 69 && mood >= 60) {
+                return "Lonely";
+            } else {
+                return "Depressed";
+            }
+        }
 };
 
 class Dog : public Pet { 
     private: 
-
     public: 
+        Dog(){
+            cout << "What is you dog's name: " << ends;
+            string n;
+            getline(cin,n);
+            Pet::setName(n);
 
+            Pet::setHunger(80);
+            Pet::setMood(80);
+        }
+        ~Dog() {
+            // goodbye pet, check pet journal/log
+        }
+        void eat() {
+            
+        }
+        void action() {
+
+        }
 };
 
 class Cat : public Pet { 
     private: 
 
     public: 
-    
+        Cat(){
+            cout << "What is you cat's name: " << ends;
+            string n;
+            getline(cin,n);
+            Pet::setName(n);
+
+            Pet::setHunger(80);
+            Pet::setMood(80);
+        }
+        ~Cat() {
+            // goodbye pet, check pet journal/log
+        }
+        void eat() {
+            
+        }
+        void action() {
+            
+        }
 };
 
 class Eagle : public Pet { 
     private: 
 
     public: 
+        Eagle() {
+            cout << "Your Eagle's name is Ernie.";
+            Pet::setName("Eagle");
+            Pet::setHunger(80);
+            Pet::setMood(80);
+        }
+        ~Eagle() {
+            // goodbye pet, check pet journal/log
+        }
+        void eat() {
 
+        }
+        void action() {
+            
+        }
 };
 
 int main() {
 
     return 0;
 }
+
